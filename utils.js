6581984bf44e420749b39c6a7bcde546e9a87fb5
Khaos <khaos@khaos-coders.org>
2020-02-16 00:57:09 +0100

    Implemented ALPHA blending of logo in QR Code

--- utils.js
+++ utils.js
@@ -42,9 +42,86 @@ const addQRLogo = async function(code, logo, size) {
     let x = Math.floor((codeImg.width - logoImg.width) / 2);
     let y = Math.floor((codeImg.height - logoImg.height) / 2);
 
-    codeImg.insert(logoImg, { x: x, y: y, inPlace: true });
+    blendInsert(logoImg, codeImg, x, y);
 
     return `data:image/png;base64,${codeImg.toBase64('image/png')}`;
+}
+
+/**
+ * Inserts a image into another. Pixels will be blended according to ALPHA channels
+ * @param {Image} from Image to intert
+ * @param {Image} to Image to insert to
+ * @param {number} x X location in target image
+ * @param {number} y Z location in target image
+ */
+function blendInsert(from, to, x, y) {
+    let srcPixel = [];
+    let targetPixel = [];
+    let posX = 0;
+    let posY = 0;
+    const srcAddAlpha = from.alpha === 0;
+    const targetAddAlpha = to.alpha === 0;
+    const srcChannelDiff = to.components - from.components;
+    const alphaChannel = to.components;
+    const divider = 255 * 255;
+
+    // Iterate through all pixels
+    for (let iX = 0; iX < from.width; iX++) {
+        posX = x + iX;
+        if (posX < to.width) {
+            for (let iY = 0; iY < from.height; iY++) {
+                posY = y + iY;
+                if (posY < to.height) {
+                    // Get pixel data
+                    srcPixel = from.getPixelXY(iX, iY);
+                    if (!srcAddAlpha && srcPixel[from.components] === 0) {
+                        // src ALPHA = 0
+                        continue;
+                    }
+
+                    targetPixel = to.getPixelXY(posX, posY);
+
+                    // Adjust to target channel count
+                    if (srcChannelDiff < 0) {
+                        srcPixel = srcPixel.slice(0, srcPixel.length + srcChannelDiff);
+                    } else if (srcChannelDiff > 0) {
+                        for (let i = 0; i < srcChannelDiff; i++) {
+                            srcPixel.push(srcPixel[0]);
+                        }
+                    }
+
+                    // Add ALPHA channels
+                    if (srcAddAlpha) {
+                        srcPixel.push(255);
+                    }
+                    if (targetAddAlpha) {
+                        targetPixel.push(255);
+                    }
+
+                    // Blend pixels
+                    for (let i = 0; i < targetPixel.length; i++) {
+                        if (i === alphaChannel) {
+                            targetPixel[alphaChannel] = srcPixel[alphaChannel] + (targetPixel[alphaChannel] * (255 - srcPixel[alphaChannel]) / 255)
+                        } else {
+                            targetPixel[i] = (srcPixel[i] * srcPixel[alphaChannel] / 255) + (targetPixel[i] * targetPixel[alphaChannel] * (255 - srcPixel[alphaChannel]) / divider);
+                        }
+                    }
+
+                    // Remove ALPHA channel, if added for calculation
+                    if (targetAddAlpha) {
+                        targetPixel = targetPixel.slice(0, targetPixel.length - 1);
+                    }
+
+                    // Set target pixel
+                    to.setPixelXY(posX, posY, targetPixel);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            break;
+        }
+    }
 }
 
 const readQRCode = async function(code) {
